58054e6069b4e89b677ff99336e963ab16e7eb10
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-06-17 12:46:57 +0200

    chore: Support Laravel up to 12.x.

---
+++ prettier.config.mjs
@@ -0,0 +1,28 @@
+export default {
+  // Include parentheses around a sole arrow function parameter.
+  arrowParens: "always",
+
+  // Which end of line characters to apply: <lf|crlf|cr|auto>
+  endOfLine: "lf",
+
+  // How to handle whitespaces in HTML.
+  // htmlWhitespaceSensitivity: css,
+
+  // Wrap markdown lines at printWidth characters.
+  proseWrap: "always",
+
+  // Use semicolon at eol where optional.
+  semi: true,
+
+  // Favor single quoted strings.
+  singleQuote: true,
+
+  // Default indentation width.
+  tabWidth: 4,
+
+  // Print trailing commas wherever possible when multi-line.
+  trailingComma: "all",
+
+  // Never use actual tabs for indentation.
+  useTabs: false,
+};
